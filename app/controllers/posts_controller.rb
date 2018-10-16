c8eed08501dd0b8e40cbfa82983130f468094dec
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-16 22:37:01 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,14 +21,14 @@ class PostsController < ApplicationController
   def edit
   end
 
-  def update
-    @post.update(post_params)
-    redirect_to post_path(@post)
-  end
-
   def body
     post = Post.find(params[:id])
     render plain: post.description
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
 private
