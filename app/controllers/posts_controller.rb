db1ac196d651154fdbf6ef00bbf64e20f1ee12a1
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-16 22:36:06 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,6 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def body
-    post = Post.find(params[:id])
-    render plain: post.description
-  end
-
   def show
   end
 
@@ -29,6 +24,11 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
+  end
+
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description
   end
 
 private
